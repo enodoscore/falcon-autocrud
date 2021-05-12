2f98903a1e9f369248dea72727c8822c7be8e011
cami <csmccarthy392@gmail.com>
2021-05-12 15:37:59 -0500

    use different method for "peeking" request stream

--- falcon_autocrud/middleware.py
+++ falcon_autocrud/middleware.py
@@ -58,7 +58,8 @@ class Middleware(object):
                 raise falcon.HTTPUnsupportedMediaType('This API supports only JSON-encoded requests')
 
         if req.content_type is not None and 'application/json' in req.content_type:
-            body = req.env['wsgi.input'].peek(req.content_length or 0)
+            body = req.stream.read(req.content_length or 0)
+            req._media = body
 
             if not body:
                 raise falcon.HTTPBadRequest(
